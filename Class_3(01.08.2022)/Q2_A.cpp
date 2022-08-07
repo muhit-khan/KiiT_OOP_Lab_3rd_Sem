cfe1ae70e7e88807bd3a0198fede262120d4464f
Muhit Khan <muhitkhan_bd@outlook.com>
2022-08-07 22:45:06 +0600

    Update Q2_A.cpp

--- Class_3(01.08.2022)/Q2_A.cpp
+++ Class_3(01.08.2022)/Q2_A.cpp
@@ -4,21 +4,17 @@ a. Call by value */
 //Roll: 21052943
 
 #include <iostream>  
-using namespace std;
-void swap(int a, int b)
-{
-    int temp;
-    temp = a;
-    a = b;
-    b = temp;
-}
+using namespace std;  
+
 int main()  
 {  
     int a, b;
     cout<<"Enter value of a and b: ";
 	cin>> a>> b;     
     cout<<"Before swap, a = "<<a<<" & b = "<<b<<endl;      
-    swap(a, b);
+    a = a * b;
+    b = a / b;
+    a = a / b;
     cout<<"After swap, a= "<<a<<" & b= "<<b<<endl;      
     return 0;  
 }
