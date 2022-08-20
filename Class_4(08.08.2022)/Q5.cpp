2b049da3f35f6a91cf0b883882e4a54a91657126
Muhit Khan <muhitkhan_bd@outlook.com>
2022-08-20 22:19:46 +0530

    Create Q5.cpp

---
+++ Class_4(08.08.2022)/Q5.cpp
@@ -0,0 +1,23 @@
+//5. Write a C++ program to calculate the power of any number using recursion.
+//MUHIT KHAN
+//Roll: 21052943
+
+#include<iostream>
+using namespace std;
+
+int pow(int base, int power)
+{
+    if(power != 0)
+        return base * pow(base, power - 1);
+    return 1;
+}
+
+int main()
+{
+    int base, power;
+    cout<<"Enter base and power: ";
+    cin>>base>>power;
+
+    cout<<"Result: "<<pow(base, power);
+    return 0;
+}
