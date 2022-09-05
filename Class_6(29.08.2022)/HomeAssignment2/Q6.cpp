2c134c9d1541227a04cee238d5b0b082505f83bf
Muhit Khan <muhitkhan_bd@outlook.com>
2022-09-05 14:40:24 +0530

    Update Q6.cpp

--- Class_6(29.08.2022)/HomeAssignment2/Q6.cpp
+++ Class_6(29.08.2022)/HomeAssignment2/Q6.cpp
@@ -1,20 +1,23 @@
-// WAP in c++ to enter any array and then reverse and display it
+// WAP in c++ to enter any array and display sum of its elements
 
 #include <iostream>
 using namespace std;
-
+int sum(int arr[])
+{
+    int sum = 0;
+    for (int i = 0; arr[i] != NULL; i++)
+        sum += arr[i];
+    return sum;
+}
 int main()
 {
-    int s, sum = 0;
+    int s;
     cout << "Enter size of array: ";
     cin >> s;
     int arr[s];
     cout << "Enter elements: ";
     for (int i = 0; i < s; i++)
-    {
         cin >> arr[i];
-        sum += arr[i];
-    }
-    cout << "Sum of array: " << sum << endl;
+    cout << "Sum of array: " << sum(arr);
     return 0;
 }
